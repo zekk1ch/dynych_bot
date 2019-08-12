343311a8242e2fde64fd3d1a7c615da5113fce24
Yevhenii Pykhov <y.pykhov@aimprosoft.com>
2019-08-12 12:35:44 +0300

    update

--- scripts/startServer.js
+++ scripts/startServer.js
@@ -1,5 +1,6 @@
 const app = require('../app');
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Telegram bot is listening on port 3000');
+app.listen(PORT, () => {
+    console.log(`Telegram bot is listening on port ${PORT}`);
 });
