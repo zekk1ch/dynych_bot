39017f4204543bf338c61d580efd4e1ad9650102
Yevhenii Pykhov <y.pykhov@aimprosoft.com>
2019-08-12 12:46:38 +0300

    update

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', router);
 
 app.get('/', (req, res) => {
+    console.log(req.method);
+    console.log(req.methodName);
     res.send('This is get');
 });
 app.post('/', (req, res) => {
