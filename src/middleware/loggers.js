da78a31c374577a4599496a554b41609a6a8c72e
Yevhenii Pykhov <y.pykhov@aimprosoft.com>
2019-08-12 13:43:41 +0300

    update

--- src/middleware/loggers.js
+++ src/middleware/loggers.js
@@ -1,7 +1,8 @@
 const requestLogger = (req, res, next) => {
     console.log(`\n${req.method} ${req.originalUrl}${req.body ? '' : '\n\n'}`);
     if (req.body) {
-        console.log(`\nbody:\n${req.body}`);
+        console.info(`\n\n${req.body}\n\n`, '%o');
+        console.info(`\n\n${req.body}\n\n`, '%O');
     }
 
     next();
