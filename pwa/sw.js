d42fad1cbd697c434148863796e89c881a5e25b5
Yev <yevhenii.pykhov@nure.ua>
2019-09-08 17:15:22 +0300

    update caching logic

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,5 +1,6 @@
 const CACHE_NAME = 'notes';
-const getUrlsToCache = (mode) => {
+const getUrlsToCache = () => {
+    const mode = new URL(location).searchParams.get('mode');
     const urls = [
         '/',
         '/app.bundle.js',
@@ -14,24 +15,57 @@ const getUrlsToCache = (mode) => {
             throw new Error('Environment mode must be either "development" or "production"');
     }
 };
-const handleInstall = async (urlsToCache) => {
+const handleInstall = async () => {
+    const urlsToCache = getUrlsToCache();
     const cache = await caches.open(CACHE_NAME);
     await cache.addAll(urlsToCache);
 };
+const handleActivate = async () => {
+    let promises;
+
+    const cacheNames = await caches.keys();
+    promises = cacheNames
+        .filter((cacheName) => cacheName !== CACHE_NAME)
+        .map((cacheName) => caches.delete(cacheName));
+    await Promise.all(promises);
+
+    const cache = await caches.open(CACHE_NAME);
+    const responses = await cache.matchAll();
+    const urlsToCache = getUrlsToCache();
+    promises = responses
+        .map((response) => response.url.replace(location.origin, ''))
+        .filter((url) => !urlsToCache.includes(url))
+        .map((url) => cache.delete(url));
+    await Promise.all(promises);
+};
 const handleFetch = async (req) => {
-    const response = await caches.match(req);
-    if (response) {
-        return response;
+    let response, isResponseOk;
+
+    try {
+        response = await fetch(req);
+        isResponseOk = response.ok;
+    } catch {
+        isResponseOk = false;
     }
 
-    return fetch(req);
+    if (isResponseOk) {
+        const cache = await caches.open(CACHE_NAME);
+        await cache.put(req, response.clone());
+    } else {
+        const cachedResponse = await caches.match(req);
+        if (cachedResponse) {
+            return cachedResponse;
+        }
+    }
+
+    return response;
 };
 
 self.addEventListener('install', (e) => {
-    const mode = new URL(location).searchParams.get('mode');
-    const urlsToCache = getUrlsToCache(mode);
-
-    e.waitUntil(handleInstall(urlsToCache));
+    e.waitUntil(handleInstall());
+});
+self.addEventListener('activate', (e) => {
+    e.waitUntil(handleActivate());
 });
 self.addEventListener('fetch', (e) => {
     e.respondWith(handleFetch(e.request));
